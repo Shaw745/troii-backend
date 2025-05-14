f95bec26d06e2312384b4f1c970106372098c510
shaw745 <shawbry5150@gmail.com>
2025-05-14 11:21:55 +0100

    new project

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,65 +1,89 @@
 const USER = require("../models/user");
-const bcrypt = require("bcryptjs")
+const bcrypt = require("bcryptjs");
 const generateToken = require("../helpers/generateToken");
-const {sendWelcomeEmail} = require("../email/sendEmail");
-
+const { sendWelcomeEmail } = require("../email/sendEmail");
 const handleRegister = async (req, res) => {
-  const { fullName, email, phoneNumber, role, password } = req.body;
-
+  const { fullName, email, password, phoneNumber, role } = req.body;
   try {
+    //check if user exists (email and phoneNumber)
+    //$or
     const existingUser = await USER.findOne({
       $or: [{ email: email || null }, { phoneNumber: phoneNumber || null }],
     });
     if (existingUser) {
-      return res.status(400).json({
-        message: "User with this email or phone number already exists",
-      });
+      return res
+        .status(400)
+        .json({ message: "Email or Phone number already exists" });
     }
-    const salt = await bcrypt.genSalt();
-    const hashedPassword = await bcrypt.hash(password,salt);
-      const verificationToken = generateToken()
-      const verificationTokenExpires = Date.now() + 14 * 60 * 60 * 1000
 
+    //protect users password
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    //verify process
+    const verificationToken = generateToken();
+    const verificationTokenExpires = Date.now() + 24 * 60 * 60 * 1000;
+    //save to db
     const user = await USER.create({
-     fullName, email, phoneNumber,role:role|| 'tenant', password:hashedPassword,verificationToken,verificationTokenExpires 
+      fullName,
+      email,
+      password: hashedPassword,
+      role: role || "tenant",
+      phoneNumber,
+      verificationToken,
+      verificationTokenExpires,
     });
 
-    const clientUrl = `${process.env.FRONTEND_URL}/verify-email/${verificationToken}`;
+    //Send an email
+    const clientUrl = ${process.env.FRONTEND_URL}/verify-email/${verificationToken};
     await sendWelcomeEmail({
       email: user.email,
       fullName: user.fullName,
       clientUrl,
     });
 
-    
-    return res.status(201).json({success: true, message: "user registered successfully", user
-    });
-
+    return res
+      .status(201)
+      .json({ success: true, message: "User Registered successfully", user });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: error.message });
   }
 };
 
-const handleVerifyEmail = async (req,res) =>{
-  const {token} = req.params;
-  try{
+const handleVerifyEmail = async (req, res) => {
+  const { token } = req.params;
+  try {
+    // 1. find user by token
     const user = await USER.findOne({
-      verificationToken:token,
-      verificationTokenExpires: {$gt: Date.now()},
+      verificationToken: token,
     });
-    if(!user){
-      return res.status(404).json({message: "Invalid or expired verification token", email: user.email})
+    if (!user) {
+      return res.status(404).json({ message: "Invalid Verification token" });
     }
-    user.isVerified= true
+    //2. check if token has expoired
+    if (user.verificationTokenExpires < Date.now()) {
+      return res
+        .status(400)
+        .json({ message: "Verification token has expired", email: user.email });
+    }
+
+    //3. check if user is already verified
+    if (user.isVerified) {
+      return res.status(400).json({ message: "Email is already verified" });
+    }
+
+    //mark the user as verified
+    user.isVerified = true;
     user.verificationToken = undefined;
     user.verificationTokenExpires = undefined;
     await user.save();
-    return res.status(200).json({success:true, message:"email verified successfully"})   
-  }catch(error){
+    return res.status(200).json({
+      success: true,
+      message: "Email verified successfully",
+    });
+  } catch (error) {
     console.error(error);
-    res.status(500).json({message:error.message})
   }
 };
 
-module.exports = { handleRegister, handleVerifyEmail };
+module.exports = { handleRegister, handleVerifyEmail };
