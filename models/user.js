7059a850c06ac3c704f06ecadd1b252a72c127dc
shaw745 <shawbry5150@gmail.com>
2025-05-19 12:44:19 +0100

    replacelandlord

--- models/user.js
+++ models/user.js
@@ -26,7 +26,7 @@ const userSchema = new Schema(
     },
     role: {
       type: String,
-      enum: ["tenant", "landord"],
+      enum: ["tenant", "landlord"],
       default: "tenant",
     },
     password: {
