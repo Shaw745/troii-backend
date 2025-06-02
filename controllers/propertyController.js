8e999f1c24053956293e278bbde200440866c3ba
shaw745 <shawbry5150@gmail.com>
2025-06-02 11:14:31 +0100

    added total properties

--- controllers/propertyController.js
+++ controllers/propertyController.js
@@ -90,6 +90,7 @@ const getAllProperties = async (req, res) => {
       totalPages,
       currentPage: parseInt(page),
       properties,
+      totalProperties,
     });
   } catch (error) {
     console.error(error);
